ff3e636df48b266121991bc665b9d38406ecbdcf
Marco Fuykschot <marcof@titaniumit.nl>
2025-03-26 09:10:38 +0100

    test doc

--- build.rs
+++ build.rs
@@ -41,37 +41,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         readme.write_all_at(content.as_bytes(), 0)?;
     }
 
-    let source = std::env::var("CARGO_SOURCE_DIR").unwrap_or_default();
-    let assets_path = if std::env::var("DOCS_RS").is_ok() {
-        Path::new(source.as_str())
-    } else {
-        Path::new(".")
-    };
-
-    let assets_path = assets_path.join("assets");
-
-    let assets = std::fs::read_dir(assets_path)?;
-
-    let target_dir = std::env::var("CARGO_TARGET_DIR").unwrap_or_default();
-    let target = std::env::var("TARGET").unwrap_or_default();
-    let doc_path = if std::env::var("DOCS_RS").is_ok() {
-        Path::new(target_dir.as_str()).join(target.as_str())
-    } else {
-        Path::new("target").to_path_buf()
-    };
-    let doc_path = doc_path
-        .join("doc")
-        .join("cucumber_reporter")
-        .join("assets");
-
-    fs::create_dir_all(doc_path.clone())?;
-
-    for asset in assets {
-        let asset = asset?;
-        let target_file = doc_path.join(asset.file_name());
-        println!("cargo::warning={:?} {:?}", asset, target_file);
-        std::fs::copy(asset.path(), target_file)?;
-    }
+  
 
     Ok(())
 }
